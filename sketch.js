37141dcfd14217a05feb7616533b558d97cc08dc
Saikat1462 <77276401+Saikat1462@users.noreply.github.com>
2021-04-12 17:31:22 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,13 +13,13 @@ var track, car1_img, car2_img, car3_img, car4_img;
 var f1,f2,s
 
 function preload(){
-  track = loadImage("../images/track.jpg");
+  track = loadImage("images/track.jpg");
   f2=loadImage("images/f1.png")
-  car1_img = loadImage("../images/car1.png");
-  car2_img = loadImage("../images/car2.png");
-  car3_img = loadImage("../images/car3.png");
-  car4_img = loadImage("../images/car4.png");
-  ground = loadImage("../images/ground.png");
+  car1_img = loadImage("images/car1.png");
+  car2_img = loadImage("images/car2.png");
+  car3_img = loadImage("images/car3.png");
+  car4_img = loadImage("images/car4.png");
+  ground = loadImage("images/ground.png");
   s=loadSound("sound/sliding.mp3")
 }
 
